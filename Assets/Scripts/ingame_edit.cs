e8e78d740ab168c1892288445f224dca849e4317
Andrew Sherman <anda02@gmail.com>
2017-07-20 15:11:43 +0100

    Continued script additions

--- Assets/Scripts/ingame_edit.cs
+++ Assets/Scripts/ingame_edit.cs
@@ -10,6 +10,7 @@ public class ingame_edit : MonoBehaviour {
 	public Text lookatname;
 	public GameObject tutorial;
 	public float moveamt = 0.1f;
+	public float rotateamt = 0.1f;
 	Text text1;
 	Text text2;
 	string string1;
@@ -24,6 +25,10 @@ public class ingame_edit : MonoBehaviour {
 	// Update is called once per frame
 	void Update() {
 		selectedname.text = selectedobj.transform.name;
+		if (Input.GetKeyDown (KeyCode.P)) {
+			Rigidbody rb = selectedobj.AddComponent<Rigidbody> () as Rigidbody;
+		}
+
 		if (edit == false && Input.GetKeyDown (KeyCode.T)) {
 			edit = true;
 			print ("edit is now true");
@@ -31,37 +36,34 @@ public class ingame_edit : MonoBehaviour {
 		else if (edit == true) {
 			tutorial.SetActive (true);
 			print ("Fuck me");
-			if (Input.GetKeyDown (KeyCode.Keypad8)){
+			if (Input.GetKey (KeyCode.KeypadPlus)){
 				selectedobj.transform.position = new Vector3(selectedobj.transform.position.x, selectedobj.transform.position.y+moveamt,selectedobj.transform.position.z);
 			}
-			if (Input.GetKeyDown (KeyCode.Keypad2)){
+			if (Input.GetKey (KeyCode.KeypadMinus)){
 				selectedobj.transform.position = new Vector3(selectedobj.transform.position.x, selectedobj.transform.position.y-moveamt,selectedobj.transform.position.z);
 			}
-			if (Input.GetKeyDown (KeyCode.Keypad4)){
+			if (Input.GetKey (KeyCode.Keypad4)){
 				selectedobj.transform.position = new Vector3(selectedobj.transform.position.x+moveamt, selectedobj.transform.position.y,selectedobj.transform.position.z);
 			}
-			if (Input.GetKeyDown (KeyCode.Keypad6)){
+			if (Input.GetKey (KeyCode.Keypad6)){
 				selectedobj.transform.position = new Vector3(selectedobj.transform.position.x-moveamt, selectedobj.transform.position.y,selectedobj.transform.position.z);
 			}
-			if (Input.GetKeyDown (KeyCode.Keypad8)){
-				selectedobj.transform.position = new Vector3(selectedobj.transform.position.x, selectedobj.transform.position.y,selectedobj.transform.position.z);
+			if (Input.GetKey (KeyCode.Keypad8)){
+				selectedobj.transform.position = new Vector3(selectedobj.transform.position.x, selectedobj.transform.position.y,selectedobj.transform.position.z+moveamt);
 			}
-			if (Input.GetKeyDown (KeyCode.Keypad8)){
-				selectedobj.transform.position = new Vector3(selectedobj.transform.position.x, selectedobj.transform.position.y,selectedobj.transform.position.z);
+			if (Input.GetKey (KeyCode.Keypad5)){
+				selectedobj.transform.position = new Vector3(selectedobj.transform.position.x, selectedobj.transform.position.y,selectedobj.transform.position.z-moveamt);
 			}
-			if (Input.GetKeyDown (KeyCode.Keypad8)){
-				selectedobj.transform.position = new Vector3(selectedobj.transform.position.x, selectedobj.transform.position.y,selectedobj.transform.position.z);
+			if (Input.GetKey (KeyCode.Keypad7)){
+				selectedobj.transform.eulerAngles = new Vector3(selectedobj.transform.eulerAngles.x+rotateamt, selectedobj.transform.eulerAngles.y,selectedobj.transform.eulerAngles.z);
 			}
-			if (Input.GetKeyDown (KeyCode.Keypad8)){
-				selectedobj.transform.position = new Vector3(selectedobj.transform.position.x, selectedobj.transform.position.y,selectedobj.transform.position.z);
+			if (Input.GetKey (KeyCode.Keypad9)){
+				selectedobj.transform.eulerAngles = new Vector3(selectedobj.transform.eulerAngles.x, selectedobj.transform.eulerAngles.y,selectedobj.transform.eulerAngles.z+rotateamt);
 			}
-			if (Input.GetKeyDown (KeyCode.Keypad8)){
-				selectedobj.transform.position = new Vector3(selectedobj.transform.position.x, selectedobj.transform.position.y,selectedobj.transform.position.z);
+			if (Input.GetKey (KeyCode.KeypadDivide)){
+				selectedobj.transform.eulerAngles = new Vector3(selectedobj.transform.eulerAngles.x, selectedobj.transform.eulerAngles.y+rotateamt,selectedobj.transform.eulerAngles.z);
 			}
-			if (Input.GetKeyDown (KeyCode.Keypad8)){
-				selectedobj.transform.position = new Vector3(selectedobj.transform.position.x, selectedobj.transform.position.y,selectedobj.transform.position.z);
-			}
-			if (Input.GetKeyDown (KeyCode.T)) {
+			if (Input.GetKey (KeyCode.Tab) && edit == true) {
 				tutorial.SetActive (false);
 				edit = false;
 			}
